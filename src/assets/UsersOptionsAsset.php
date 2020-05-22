8d016320e028ee025a162d356c545289b2d9e11a
Pozitronik <antikillerxxl@gmail.com>
2020-05-22 23:45:09 +0300

    void return types

--- src/assets/UsersOptionsAsset.php
+++ src/assets/UsersOptionsAsset.php
@@ -12,7 +12,7 @@ class UsersOptionsAsset extends AssetBundle {
 	/**
 	 * @inheritdoc
 	 */
-	public function init() {
+	public function init():void {
 		$this->sourcePath = __DIR__.'/';
 		$this->js = ['js/users-options.js'];
 		$this->publishOptions = ['forceCopy' => false];
